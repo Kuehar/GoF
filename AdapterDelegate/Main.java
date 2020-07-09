f51305136b38a09e6df489acdefe74ddc8ae1620
Kuehar <green95251@gmail.com>
2020-07-10 01:32:44 +0900

    added AdapterDelegate

---
+++ AdapterDelegate/Main.java
@@ -0,0 +1,9 @@
+package AdapterDelegate;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Good Morning!!");
+        p.printWeak();
+        p.printStrong();
+    }
+}
